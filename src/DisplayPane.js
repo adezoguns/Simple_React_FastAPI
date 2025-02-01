2ff815b7d866f146ecc11ab04c97c0eac73be9c6
Ogunleye Adeola <adeola.ogunleye@atheneum-partners.com>
2025-02-01 23:38:55 +0000

    Worked on the displaypane css to change the look and feel

--- src/DisplayPane.js
+++ src/DisplayPane.js
@@ -1,59 +1,7 @@
 import { useEffect, useState } from "react";
 import { eventBus } from "./emitter";
+import { state_dict } from "./DataContext";
 
-const state_dict = {
-  AL: "Alabama",
-  AK: "Alaska",
-  AZ: "Arizona",
-  AR: "Arkansas",
-  CA: "California",
-  CO: "Colorado",
-  CT: "Connecticut",
-  DE: "Delaware",
-  DC: "District of Columbia",
-  FL: "Florida",
-  GA: "Georgia",
-  HI: "Hawaii",
-  ID: "Idaho",
-  IL: "Illinois",
-  IN: "Indiana",
-  IA: "Iowa",
-  KS: "Kansas",
-  KY: "Kentucky",
-  LA: "Louisiana",
-  ME: "Maine",
-  MD: "Maryland",
-  MA: "Massachusetts",
-  MI: "Michigan",
-  MN: "Minnesota",
-  MS: "Mississippi",
-  MO: "Missouri",
-  MT: "Montana",
-  NE: "Nebraska",
-  NV: "Nevada",
-  NH: "New Hampshire",
-  NJ: "New Jersey",
-  NM: "New Mexico",
-  NY: "New York",
-  NC: "North Carolina",
-  ND: "North Dakota",
-  OH: "Ohio",
-  OK: "Oklahoma",
-  OR: "Oregon",
-  PA: "Pennsylvania",
-  RI: "Rhode Island",
-  SC: "South Carolina",
-  SD: "South Dakota",
-  TN: "Tennessee",
-  TX: "Texas",
-  UT: "Utah",
-  VT: "Vermont",
-  VA: "Virginia",
-  WA: "Washington",
-  WV: "West Virginia",
-  WI: "Wisconsin",
-  WY: "Wyoming",
-};
 
 function DisplayPane({ getRows }) {
   const [items, setItems] = useState([]);
@@ -61,10 +9,10 @@ function DisplayPane({ getRows }) {
 
   useEffect(() => {
     eventBus.on("reload", () => setItems(JSON.parse(localStorage.getItem("apiData"))));
-    return () => eventBus.removeAllListeners("reload");
   }, []);
 
-  console.log(items);
+  //console.log(items);
+  console.log(selectedItems);
 
   // Handle checkbox toggle
   const handleSelectionChange = (id) => {
